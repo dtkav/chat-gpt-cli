33d1ac0275adf3c70641f082bc72d89ef6f8543a
PopDaddyGames <36561261+PopDaddyGames@users.noreply.github.com>
2022-12-13 11:22:01 -0700

    Update ChatGPT-Terminal.py

--- ChatGPT-Terminal.py
+++ ChatGPT-Terminal.py
@@ -1,28 +1,35 @@
+# Import the necessary modules
 import openai
 import os
 import getpass
 
+# Set the API key and some initial parameter values
 api_key = ""
 temperature = 0.5 # 0 to 1
 max_tokens = 2000 # max 4000
 personality = ""
 
+# Check the operating system and set the appropriate command to clear the terminal screen
 if os.name == 'nt':
     clear = 'cls'
 else:
     clear = 'clear'
 
+# Define a function to clear the terminal screen
 def clearScreen():
   os.system(clear)
   print("===== ChatGPT-Terminal =====")
 
+# Call the function to clear the terminal screen
 clearScreen()
 
+# Prompt the user for their API key if it hasn't been provided in the code already
 if api_key == "":
   openai.api_key = getpass.getpass(prompt="OpenAI API Key")
 else:
   openai.api_key = api_key
 
+# Enter a while loop to continually prompt the user for input and generate responses from the OpenAI API
 while True:
   response = openai.Completion.create(
     model="text-davinci-003",
@@ -34,4 +41,5 @@ while True:
     presence_penalty=0.0
   )
 
+  # Print the generated response
   print(">> " + response["choices"][0]["text"].strip())
